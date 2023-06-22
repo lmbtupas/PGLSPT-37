be5976e79dd2871bf2ff07c42f2264d042ef9ee6
Yancy <yancy@symph.co>
2023-06-22 21:58:24 +0800

    Added main logotype to hero page

--- pglspt-37/components/hero.jsx
+++ pglspt-37/components/hero.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Hero = () => {
     return (
         <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
             {/* Overlay */}
             <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#000F34]/80 z-[2]'/>
-            <div className='flex flex-col z-[2] justify-center items-center gap-4'>
+            <div className='flex flex-col z-[2] justify-center items-center mt-16'>
                 <div className='text-white font-header text-3xl'>
                     PIGLAS 37
                 </div>
-                <div className='text-yellow font-header text-6xl'>
-                    Chase our Purpose
+                <div>
+                <Image
+                    src="/images/COP.png"
+                    width={500}
+                    height={500}
+                    alt="chase our purpose logotype"
+                    unoptimized
+                    />
                 </div>
-                <div className='text-white text-center font-body text-xl w-96'>
+                <div className='text-white text-center font-body text-xl w-[40rem] mt-8'>
                     Lorem ipsum dolor sit amet consectetur. Condimentum mattis et proin massa. Molestie feugiat sem mauris risus sed. Fermentum metus urna enim arcu pharetra sem faucibus. Aenean aenean mauris quis proin.
                 </div>
             </div>
