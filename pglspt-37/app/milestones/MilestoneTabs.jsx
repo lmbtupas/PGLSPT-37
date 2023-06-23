a316da8d7c42d9adc06fe7b60bfef68ef143c61c
Angelli <akmalkuino@addu.edu.ph>
2023-06-23 16:11:33 +0800

    instal MUI dependencies and add placeholder code for  milestone tabs

---
+++ pglspt-37/app/milestones/MilestoneTabs.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Box, Tab } from '@mui/material'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
+import { useState } from 'react'
+
+export default function MilestoneTabs() {
+    const [value, setValue] = useState('1')
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+  return (
+    <Box>
+        <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList
+            onChange={handleChange}
+            aria-label='Tabs example'
+            textColor='secondary'
+            indicatorColor='secondary'
+            centered
+            variant='scrollable'
+            scrollButtons='auto'>
+            <Tab
+                iconPosition='start'
+                label='Tab One'
+                value='1'
+            />
+            <Tab label='Tab Two' value='2' disabled />
+            <Tab label='Tab Three' value='3' />
+            <Tab label='Tab Four' value='4' />
+            <Tab label='Tab Five' value='5' />
+            <Tab label='Tab Six' value='6' />
+            </TabList>
+        </Box>
+        <TabPanel value='1'>Item One</TabPanel>
+        <TabPanel value='2'>Item Two</TabPanel>
+        <TabPanel value='3'>Item Three</TabPanel>
+        <TabPanel value='4'>Item Four</TabPanel>
+        <TabPanel value='5'>Item Five</TabPanel>
+        <TabPanel value='6'>Item Six</TabPanel>
+        </TabContext>
+    </Box>
+  )
+}
--- pglspt-37/app/milestones/page.js
+++ pglspt-37/app/milestones/page.js
@@ -1,3 +1,5 @@
+import MilestoneTabs from "./MilestoneTabs"
+
 
 export default function page() {
   return (
@@ -5,15 +7,7 @@ export default function page() {
             {/* Overlay */}
             <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#000F34]/80 z-[2]'/>
             <div className='flex flex-col z-[2] justify-center items-center gap-4'>
-                <div className='text-white font-header text-3xl'>
-                    PIGLAS 37
-                </div>
-                <div className='text-yellow font-header text-6xl'>
-                    Chase our Purpose
-                </div>
-                <div className='text-white text-center font-body text-xl w-96'>
-                    Lorem ipsum dolor sit amet consectetur. Condimentum mattis et proin massa. Molestie feugiat sem mauris risus sed. Fermentum metus urna enim arcu pharetra sem faucibus. Aenean aenean mauris quis proin.
-                </div>
+                <MilestoneTabs/>
             </div>
         </div>
   )
