910d3eba07edfe82ffdbf391763b6b81d5689758
Yancy <yancy@symph.co>
2023-06-26 20:34:58 +0800

    added carousel component for development

---
+++ pglspt-37/components/carousel.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-key */
+'use client';
+
+import React, {useState} from "react";
+
+const Carousel = () => {
+    return (
+        <div />
+    )
+}
+
+export default Carousel
