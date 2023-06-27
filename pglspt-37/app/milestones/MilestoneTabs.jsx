8db4dde3d69a2b4a564849918a3e21ab3eaa0805
Angelli <akmalkuino@addu.edu.ph>
2023-06-28 07:03:44 +0800

    add milestone tabs

--- pglspt-37/app/milestones/MilestoneTabs.jsx
+++ pglspt-37/app/milestones/MilestoneTabs.jsx
@@ -3,44 +3,46 @@
 import { Box, Tab } from '@mui/material'
 import { TabContext, TabList, TabPanel } from '@mui/lab'
 import { useState } from 'react'
+import data from '../../data/milestonesData.json'
 
 export default function MilestoneTabs() {
-    const [value, setValue] = useState('1')
+    const [value, setValue] = useState(0)
 
     const handleChange = (event, newValue) => {
         setValue(newValue)
     }
   return (
-    <Box>
+    <div className='flex felx-row w-full'>
         <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <div>
             <TabList
+            orientation='vertical'
             onChange={handleChange}
             aria-label='Tabs example'
             textColor='secondary'
             indicatorColor='secondary'
-            centered
-            variant='scrollable'
-            scrollButtons='auto'>
-            <Tab
-                iconPosition='start'
-                label='Tab One'
-                value='1'
-            />
-            <Tab label='Tab Two' value='2' disabled />
-            <Tab label='Tab Three' value='3' />
-            <Tab label='Tab Four' value='4' />
-            <Tab label='Tab Five' value='5' />
-            <Tab label='Tab Six' value='6' />
+            // centered
+            // variant='scrollable'
+            scrollButtons='auto'
+            sx={{ borderRight: 1, borderColor: 'divider', width: '12rem' }}
+            >
+            {
+                data.items.map((item, index) => (
+                    <Tab value = {index} label = {item.title} sx={{alignItems: 'end', textAlign: 'right'}}/>
+                ))
+            }
             </TabList>
-        </Box>
-        <TabPanel value='1'>Item One</TabPanel>
-        <TabPanel value='2'>Item Two</TabPanel>
-        <TabPanel value='3'>Item Three</TabPanel>
-        <TabPanel value='4'>Item Four</TabPanel>
-        <TabPanel value='5'>Item Five</TabPanel>
-        <TabPanel value='6'>Item Six</TabPanel>
+        </div>
+        <div>
+            {data.items.map((item, index) => {
+                return (
+                <TabPanel value={index}>
+                    {item.text}
+                </TabPanel>
+                );
+            })}
+        </div>
         </TabContext>
-    </Box>
+    </div>
   )
 }
--- pglspt-37/app/milestones/page.js
+++ pglspt-37/app/milestones/page.js
@@ -3,10 +3,10 @@ import MilestoneTabs from "./MilestoneTabs"
 
 export default function page() {
   return (
-    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom_img">
+    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover">
             {/* Overlay */}
-            <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#000F34]/80 z-[2]'/>
-            <div className='flex flex-col z-[2] justify-center items-center gap-4'>
+            {/* <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#000F34]/80 z-[2]'/> */}
+            <div className='mt-8 flex flex-col w-6/12 z-[2]'>
                 <MilestoneTabs/>
             </div>
         </div>
