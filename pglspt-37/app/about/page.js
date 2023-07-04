9a04df8b5bc828153a0f78695d24e133b6e2e9a0
Yancy <yancy@symph.co>
2023-07-04 14:40:05 +0800

    changed name of components for naming convention

--- pglspt-37/app/about/page.js
+++ pglspt-37/app/about/page.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image"
 import Slider from "@/components/carousel"
-import { CarouselData } from "@/components/carousel_data"
 
 export default function About() {
   return (
@@ -31,9 +30,35 @@ export default function About() {
         </div>
       </section>
       
-      <section className="flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover custom_img">
-        <div className='absolute bg-[#000F34]/80 z-[2] h-screen w-full' />
-          PIGLAS 37
+      <section className="flex flex-col items-center w-full h-auto bg-fixed bg-center bg-cover custom_img">
+        <div className='absolute bg-[#000F34]/80 z-[2] h-[1210px] w-full' />
+        <div className="flex flex-col justify-center items-center z-[3] mt-16">
+            <div className='text-white font-header text-3xl'>
+                PIGLASAPAT 37
+            </div>
+            
+            <div className='px-8'>
+                <Image
+                    src="/images/COP.png"
+                    width={750}
+                    height={750}
+                    alt="chase our purpose logotype"
+                    priority={true}
+                    unoptimized
+                />
+            </div>
+
+            <div className="body_text w-[50rem] text-justify mt-5">
+              <p className="mt-5 subheader_1 text-center">What are we truly seeking for? </p>
+              <p className="mt-3">For 37 years, the Pinag-Isang Lakas ng Samahan ng mga Progresibong Atenista journeyed its way through the test of time. The stories of defeat, triumphs, downfall, and rebirth are what fueled the organization to continue pursuing its mission through ups and downs, crests and troughs. </p>
+              <p className="mt-3">Moreso, navigating through a world that continues to progress and circumstances that continue to evolve, we find ourselves reaching out for our ultimate purpose. </p>
+              <p className="mt-3">As it evolved over the past 37 years, the organization has become a premium vehicle for student leaders to advance their contribution to the university mission and to society - Para sa Ateneo, Para sa Lipunan! </p>
+              <p className="mt-3">PIGLASAPAT, rooted in its People, Passion, and Principle, answers today’s call of the times by actively seeking for that purpose — one that is shaped by the realities of the collective and grounded upon the aspirations of what we could become as a community. In introducing fresh traditions, stronger passion projects, and new spaces for service, we thrust forward the Phoenix agenda. We now live on a legacy to create new stories and cultures that will continue to be set in motion for even more glorious years. </p>
+              <p className="mt-3">But, a true PIGLASPAT leader is bold. So, we don’t just run towards our purpose — we chase it. We chase, even if it means going against the rapids. We chase, even if it means moving mountains. We chase, no matter how difficult the circumstances could get. A deviant from the status quo, a challenger of the norm, a  trailblazer of society — this is the Phoenix spirit we live for. </p>
+              <p className="mt-3 mb-5">On our 37th year, we venture forward to the endless possibilities ahead of us while remaining rooted to our biggest why’s. Today, we no longer wait for the perfect chances. We no longer sit idle in anticipation for change. Together, we <span className="font-header text-yellow"> #ChaseOurPurpose. </span></p>
+            </div>
+
+          </div>
       </section>
     </div>
   )
--- pglspt-37/components/button.jsx
+++
@@ -1,5 +0,0 @@
-const Button = () => {
-
-}
-
-export default Button
