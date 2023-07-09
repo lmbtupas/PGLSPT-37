9a4f1be874a9b615070654375c239a9fbea1ae27
Yancy <yancy@symph.co>
2023-07-09 14:21:19 +0800

    minor changes

--- pglspt-37/app/page.js
+++ pglspt-37/app/page.js
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import Hero from './hero/hero'
 import About from './about/page'
 import Activities from './activities/page'
+import Page from './milestones/page'
 
 export default function Home() {
   return (
     // <Hero />
   // <About />
-  <Activities />
+  // <Activities />
+  <Page />
   )
 }
