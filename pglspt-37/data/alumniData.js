cda4c6e713298a7f214a48f2b7d3addbecad4d4d
Angelli <akmalkuino@addu.edu.ph>
2023-08-06 11:34:54 +0800

    added images in milestones

---
+++ pglspt-37/data/alumniData.js
@@ -0,0 +1,126 @@
+import Cayona from "@/public/images/notableAlumni/Cayona.jpeg"
+import Torreon from "@/public/images/notableAlumni/Torreon.jpeg"
+import Lacorte from "@/public/images/notableAlumni/Lacorte.jpeg"
+import Ang from "@/public/images/notableAlumni/Ang.jpeg"
+import Cesar from "@/public/images/notableAlumni/Cesar.jpeg"
+import Sabate from "@/public/images/notableAlumni/Sabate.jpg"
+import Pasia from "@/public/images/notableAlumni/Pasia.jpeg"
+import Dumago from "@/public/images/notableAlumni/Dumago.jpeg"
+import Zerrudo from "@/public/images/notableAlumni/Zerrudo.jpeg"
+import Bonleon from "@/public/images/notableAlumni/Bonleon.jpeg"
+import Catcha from "@/public/images/notableAlumni/Catcha.jpeg"
+import Pang from "@/public/images/notableAlumni/Pang.jpeg"
+import Macaraeg from "@/public/images/notableAlumni/Macaraeg.jpeg"
+import Pinzon from "@/public/images/notableAlumni/Pinzon.jpeg"
+import Sinsuat from "@/public/images/notableAlumni/Sinsuat.jpg"
+
+
+
+
+export const alumniData = [
+    {
+        "title" : "SAMAHAN Presidents",
+        "officers" : [
+            {
+                "name": "Samantha Claire Cayona",
+                "position": "SAMAHAN President 2022-2023",
+                "img": Cayona
+            },
+            {
+                "name": "Karlo Alessandro Torreon",
+                "position": "SAMAHAN President 2021-2022",
+                "img": Torreon
+            },
+            {
+                "name": "Renz Allan Lacorte",
+                "position": "SAMAHAN President 2020-2021",
+                "img": Lacorte
+            },
+            {
+                "name": "Christian Dominic Ang",
+                "position": "SAMAHAN President 2019-2020",
+                "img": Ang
+            },
+            {
+                "name": "Jerry Louise Huerbana",
+                "position": "SAMAHAN President 2018-2019",
+                "img": Cayona
+            }
+        ]
+    },
+    {
+        "title" : "Class Valedictorians",
+        "officers" : [
+            {
+                "name": "Nina Patricia Cesar",
+                "position": "Valedictorian, Batch 2019",
+                "img": Cesar
+            },
+            {
+                "name": "Miles Galicia",
+                "position": "Valedictorian, Batch 2017",
+                "img": Cayona
+            },
+            {
+                "name": "Kurt Sabate",
+                "position": "Valedictorian, Batch 2016",
+                "img": Sabate
+            },
+            {
+                "name": "Jubail Wee Pasia",
+                "position": "Valedictorian, Batch 2014",
+                "img": Pasia
+            },
+            {
+                "name": "Aldwin Dumago",
+                "position": "Valedictorian, Batch 2012",
+                "img": Dumago
+            }
+        ]
+    },
+    {
+        "title" : "2022 National Topnotchers",
+        "officers" : [
+            {
+                "name": "Kyrr Justino Zerrudo",
+                "position": "Top 3, Philippine Nursing Licensure Exam May 2022",
+                "img": Zerrudo
+            },
+            {
+                "name": "Martin Bonleon",
+                "position": "Top 6, Electronics Engineering Licensure Exam October 2022\nTop 3, Electronics Technician Board Exam October 2022",
+                "img": Bonleon
+            },
+            {
+                "name": "Alvielle Izza Catcha",
+                "position": "Top 3, Certified Public Accountant Licensure Exam October 2022",
+                "img": Catcha
+            },
+            {
+                "name": "Margaux Pang",
+                "position": "Top 15, Philippine Nursing Licensure Exam May 2022",
+                "img": Pang
+            }
+        ]
+    },
+    {
+        "title" : "National/International Ventures",
+        "officers" : [
+            {
+                "name": "Trixy Marie Macaraeg",
+                "position": "University Scholars Leadership Symposium - Kuala Lumpur Malaysia, August 2019",
+                "img": Macaraeg
+            },
+            {
+                "name": "Khryzza Mae Pinzon-Kelley",
+                "position": "First Mindanaoan Chairperson of the Student Council Alliance of the Philippines, 2016",
+                "img": Pinzon
+            },
+            {
+                "name": "Jorjani Sinsuat",
+                "position": "UNODC Regional Conference on Preventing Violent Extremisim - Thailand, 2019",
+                "img": Sinsuat
+            }
+        ]
+    }
+]
---
+++ pglspt-37/data/milestonesData.js
@@ -0,0 +1,75 @@
+import BalikSilid from "@/public/images/milestones/BalikSilid.jpg"
+import BilangBahagi from "@/public/images/milestones/BilangBahagi.jpg"
+import fylp from "@/public/images/milestones/FYLP.jpeg"
+import Layag from "@/public/images/milestones/Layag.jpeg"
+import PrideMarch from "@/public/images/milestones/PrideMarch.jpeg"
+import ProjectKaramay from "@/public/images/milestones/ProjectKaramay.jpeg"
+import TulongSkwela from "@/public/images/milestones/TulongSkwela.jpeg"
+
+import img1 from '@/public/images/hero_1.jpg'
+
+export const milestonesData = [
+    {
+        "title": "Layag Pilipinas",
+        "date": "2020 - Present",
+        "text": "Now on its third leg, Layag Pilipinas: The Lead PH National Youth Summit serves as an avenue to convene student leaders all over the country to engage with the nation's leading voices in different sectors of society. Notable invited speakers include Atty. Chel Diokno and Atty. Leni Robredo last 2020 and Hon. Councilor Luna Acosta last 2022.",
+        "img": Layag,
+        "link": "https://www.facebook.com/layagphilippines20"
+    },
+    {
+        "title": "Tulong Skwela",
+        "date": "2020 - Present",
+        "text": "For the past two years, Tulong Skwela PH has provided educational materials, supplies, and connectivity devices to more than 10 partner public schools and reached 5,769 beneficiaries across different regions in Mindanao. The project was also heralded as one of the 39 Ayala Malls' Extra Mile Changemakers during the pandemic last 2020.",
+        "img": TulongSkwela,
+        "link": "https://www.facebook.com/tulongskwela2022"
+    },
+    {
+        "title": "Pride March",
+        "date": "2018",
+        "text": "PGLS co-spearheaded the first-ever Pride March in AdDU in hopes of contributing to the fight for a wider acceptance of the LGBTQIA+ community and their rights.",
+        "img": PrideMarch,
+        "link": "#"
+    },
+    {
+        "title": "2nd Eco-Congress",
+        "date": "2018",
+        "text": "PGLS initiated the second installment of the AdDU Eco Congress last 2018, which resulted in resolutions crafted by Atenean delegates to lobby eco-friendly policies and programs for the environment.",
+        "img": img1,
+        "link": "#"
+    },
+    {
+        "title": "Filipino Youth Leadership Program",
+        "date": "2021",
+        "text": "FYLP is a month-long leadership development experience to learn directly from the nation's key storytellers, esteemed lecturers, and young-driven mentors by 30 sterling student leaders of Mindanao.",
+        "img": fylp,
+        "link": "#"
+    },
+    {
+        "title": "Balik Silid PH",
+        "date": "2021",
+        "text": "Balik Silid PH is a movement that fought for the inclusion of Higher Ed students in the priority list of vaccination programs in the Philippines. 7,197 signatories signed the petition.",
+        "img": BalikSilid,
+        "link": "https://www.facebook.com/baliksilidph"
+    },
+    {
+        "title": "Bilang Bahagi",
+        "date": "2021",
+        "text": "We co-presented Bilang Bahagi, which was a voters registration movement that aimed to raise the number of registered Mindanaoan voters who will participate in the Halalan 2022.",
+        "img": BilangBahagi,
+        "link": "https://www.facebook.com/habipilipinas2022"
+    },
+    {
+        "title": "Sagana",
+        "date": "2022",
+        "text": "PIGLASAPAT initiates its own adopt-a-hectare program at the Ayala Carbon Forest, Davao City, and commits to nurturing its seedlings in contribution to environmental conservation.",
+        "img": img1,
+        "link": "#"
+    },
+    {
+        "title": "Project Karamay",
+        "date": "2022",
+        "text": "In celebration of National Mental Health Month, PIGLASAPAT joins the crusade of raising mental health awareness through conversations that unravel how college students cope and remain mentally healthy.",
+        "img": ProjectKaramay,
+        "link": "https://www.facebook.com/ateneopiglas/posts/pfbid02kpVQruoPSpQuVULxECh1JcF43gcU1cUFv6PSsKFYausjUZ6dL2bmHYzPhh3CnBTBl"
+    }
+]
