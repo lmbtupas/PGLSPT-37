fdb07f283c6063259116fdc78e92011394968a28
Yancy <yancy@symph.co>
2023-09-01 03:32:17 +0800

    added script to hide finished month

--- pglspt-37/app/activities/EventCard.jsx
+++ pglspt-37/app/activities/EventCard.jsx
@@ -1,12 +1,9 @@
 export default function EventCard({title, date}) {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-white rounded-lg p-3 w-[60rem]">
-        <div className="text-blue font-header text-2xl text-center md:text-left">
+    <div className="flex flex-col md:flex-row items-center justify-center bg-white rounded-lg p-3 w-[60rem]">
+        <div className="text-blue font-header text-2xl text-center">
             {title}
         </div>
-        <div className="text-blue font-body text-2xl">
-            {date}
-        </div> 
     </div>
   )
 }
--- pglspt-37/app/activities/page.js
+++ pglspt-37/app/activities/page.js
@@ -1,35 +1,56 @@
 'use client'
 
+import React from 'react';
 import Background from '@/components/Background';
-import bg from '@/public/images/backgroundImages/activitiesbg.jpg'
+import bg from '@/public/images/backgroundImages/activitiesbg.jpg';
 import EventCard from './EventCard';
-import data from '@/data/eventData.json'
-import Image from 'next/image';
+import data from '@/data/eventData.json';
 
 export default function Activities() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth(); // Add 1 to make months 1-indexed
+  const currentYear = currentDate.getFullYear();
+
+  const isAfterCurrentMonthYear = (eventMonth, eventYear) => {
+    return eventYear > currentYear || (eventYear === currentYear && eventMonth >= currentMonth);
+  };
+
   return (
     <Background bgImg={bg}>
       <div className='header_2 text-center mt-12 mb-24'>
         UPCOMING ACTIVITIES
       </div>
-    {
-      data.events.map((item, index)=>(
-        <div key={index} className='flex flex-col mb-24'>
-          <div className='text-center mb-8 subheader_2'>
-            {item.month}
-          </div>
-          <div>
-          <div className='flex flex-row flex-wrap justify-center mx-4 gap-8'>
-            {
-              item.event.map((item,index)=>(
-                <EventCard key={index} title={item.title} date={item.date} />
-              ))
-            }
-          </div>
-        </div>
-        </div>
-      ))
-    }
+      {
+        data.events.map((item, index) => {
+          const eventMonth = item.month_num;
+          const eventYear = item.year;
+          const month_name = item.month;
+
+          if (isAfterCurrentMonthYear(eventMonth, eventYear)) {
+            return (
+              <div key={index} className='flex flex-col mb-24'>
+                <div className='text-center mb-8 subheader_2'>
+                  {month_name} {eventYear}
+                </div>
+                <div>
+                  <div className='flex flex-row flex-wrap justify-center mx-4 gap-8'>
+                    {
+                      item.event.map((event, eventIndex) => (
+                        <EventCard
+                          key={eventIndex}
+                          title={event.title}
+                          date={event.date}
+                        />
+                      ))
+                    }
+                  </div>
+                </div>
+              </div>
+            );
+          }
+          return null; // Hide events for months before the current month and year
+        }).filter(Boolean) // Filter out null values
+      }
     </Background>
-  )
+  );
 }
