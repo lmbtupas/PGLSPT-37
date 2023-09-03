1fb61c331cb9453c4c238a12463277ce5197b251
Yancy <yancy@symph.co>
2023-09-03 23:06:18 +0800

    redid entire about page

--- pglspt-37/app/about/page.js
+++ pglspt-37/app/about/page.js
@@ -2,67 +2,80 @@
 import Image from "next/image"
 import Slider from "@/components/Carousel"
 import Background from "@/components/Background"
-import bg from '@/public/images/backgroundImages/aboutbg.jpg'
+import bg from '@/public/images/backgroundImages/PGLS-49.jpg'
 
 export default function About() {
   return (
     <div className="">
-      <section className="flex flex-col items-center justify-center">
-        <Background bgImg={bg}>
-          <div className="z-[3] flex flex-col items-center justify-center">
-            <h2 className="header_2"> PIGLASAPAT </h2>
-            <h3 className="subheader_1 text-center uppercase px-6" > Pinag-isang Lakas ng Samahan ng mga Progresibong Atenista </h3>
-            <div className="body_text w-[30rem] md:w-[40rem] lg:w-[50rem] text-justify mt-5 px-6 md:px-0">
-              <p className="mt-3"> The Pinag-isang Lakas ng Samahan ng mga Progresibong Atenista or PIGLASAPAT is the longest-running and premier socio-civic and political organization of Ateneo de Davao University established in 1986. </p>
-              <p className="mt-3">Formed during times of national distress brought by Martial Law, the organization has propelled resilience over the past decades. It became a leading voice that echoed the students' call for social justice. </p>
-              <p className="mt-3">As it evolved over the past 37 years, the organization has become a premium vehicle for student leaders to advance their contribution to the university mission and to society - Para sa Ateneo, Para sa Lipunan! </p>
-              <p className="mt-5 subheader_1 text-center">This is #PGLS37. We are PIGLASAPAT. </p>
-            </div>
+      <Background bgImg={bg}>
+        <section className="flex flex-col justify-center items-center gap-6">
+          <div className="px-6 justify-center align-middle text-center">
+            <h1 className="font-header text-yellow text-5xl">PIGLASAPAT</h1>
+            <h3 className="font-header text-white text-xl">Pinag-isang Lakas ng Samahan ng mga Progresibong Atenista</h3>
           </div>
-        </Background>
-      </section>
-      
-      <section className="flex flex-col items-center justify-center mt-[2rem] md:mt-[5rem]">
-        <div className="flex flex-col items-center justify-center mt-20 md:mt-1">
-          <div className="z-[3] flex flex-col items-center justify-center">
-            <h2 className="header_2"> Our Pillars </h2>
-            <h2 className="subheader_1 text-center uppercase"> A PIGLASAPAT Leader Adheres to the Three Pillars  </h2>
-              <div className="w-fit">
-                <Slider />
-              </div>
+          <div className="px-6 md:px-40 justify-center align-middle items-center font-body font-normal text-white md:text-2xl">
+            <p className="text-justify">The Pinag-isang Lakas ng Samahan ng mga Progresibong Atenista or PIGLASAPAT is the longest-running and premier socio-civic and political organization of Ateneo de Davao University established in 1986. </p>
+            <p className="text-justify mt-5">Formed during times of national distress brought by Martial Law, the organization has propelled resilience over the past decades. It became a leading voice that echoed the students' call for social justice.</p>
+            <p className="text-justify mt-5">As it evolved over the past 37 years, the organization has become a premium vehicle for student leaders to advance their contribution to the university mission and to society - <span className="text-yellow">Para sa Ateneo, Para sa Lipunan!</span></p>
+            <h2 className="font-header text-yellow text-2xl text-center mt-5">This is #PGLS37. We are PIGLASAPAT.</h2>
           </div>
-        </div>
-      </section>
-      
-      <section className="flex flex-col items-center w-full mt-[10rem] md:mt-[5rem]">
-        <div className="flex flex-col justify-center items-center z-[3]">
-            <div className='text-white font-header text-3xl'>
-                PIGLASAPAT 37
-            </div>
-            
-            <div className='px-8'>
+        </section>
+
+        <section className="flex flex-col justify-center items-center gap-6 mt-10">
+          <div className="px-6 justify-center align-middle text-center">
+            <h1 className="font-header text-yellow text-5xl">OUR PILLARS</h1>
+            <h3 className="font-header text-white text-xl">A PIGLASAPAT leader adheres to the three pillars:</h3>
+          </div>
+          <div>
+            <Slider />
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row justify-center items-center bg-white h-full px-4 md:px-12">  
+          <div className="font-body text-lg text-justify px-6 items-center justify-center align-middle">
+            <div className=" items-center justify-center">
+              <div className='mt-5'>
                 <Image
-                    src="/images/COP.png"
-                    width={750}
-                    height={750}
-                    alt="chase our purpose logotype"
-                    priority={true}
-                    unoptimized
+                  src="/images/COP.png"
+                  width={550}
+                  height={550}
+                  alt="chase our purpose logotype"
+                  priority={true}
+                  className="items-center justify-center"
+                  unoptimized
                 />
+              </div>
+            <p className="mt-5 font-italic font-extrabold">What are we truly seeking for?</p>
+            <p className="mt-5">For <span className="text-blue font-bold">37</span> years, the <span className="text-blue font-bold">Pinag-Isang Lakas ng Samahan ng mga Progresibong Atenista</span> journeyed its way through the test of time. The stories of defeat, triumphs, downfall, and rebirth are what fueled the organization to continue pursuing its mission through ups and downs, crests and troughs. </p>
+            <p className="mt-5">Moreso, navigating through a world that continues to progress and circumstances that continue to evolve, we find ourselves reaching out for our <span className="text-blue font-bold">ultimate purpose</span>. </p>
+            <p className="mt-5">PIGLASAPAT, rooted in its <span className="text-blue font-bold">People</span>, <span className="text-blue font-bold">Passion</span>, and <span className="text-blue font-bold">Principles</span>, answers today’s call of the times by actively seeking for that purpose — one that is shaped by the realities of the collective and grounded upon the aspirations of what we could become as a community. In introducing fresh traditions, stronger passion projects, and new spaces for service, we thrust forward the Phoenix agenda. We now live on a legacy to create new stories and cultures that will continue to be set in motion for even more glorious years. </p>
+            <p className="mt-5">But, a true PIGLASPAT leader is <span className="text-blue font-bold">bold</span>. So, we don’t just run towards our purpose — <span className="text-blue font-bold">we chase it</span>. We chase, even if it means going against the rapids. We chase, even if it means moving mountains. We chase, no matter how difficult the circumstances could get. A deviant from the status quo, a challenger of the norm, a  trailblazer of society — this is the <span className="text-blue font-bold">Phoenix spirit</span> we live for. </p>
+            <p className="mt-5">On our 37th year, we venture forward to the endless possibilities ahead of us while remaining rooted to our biggest why’s. Today, we no longer wait for the perfect chances. We no longer sit idle in anticipation for change.</p>
+          
+              <div className="mt-5 flex flex-row align-middle justify-center md:justify-normal items-center mb-5">
+                <p className="text-blue font-bold mr-3 "> Together we,</p>
+                <Image
+                  src="/images/COP.png"
+                  width={250}
+                  height={250}
+                  alt="chase our purpose logotype"
+                  priority={true}
+                  optimized
+                />
+              </div>
             </div>
-
-            <div className="body_text w-[30rem] md:w-[40rem] lg:w-[50rem] text-justify mt-5 px-6 md:px-0">
-              <p className="mt-5 subheader_1 text-center">What are we truly seeking for? </p>
-              <p className="mt-3">For 37 years, the Pinag-Isang Lakas ng Samahan ng mga Progresibong Atenista journeyed its way through the test of time. The stories of defeat, triumphs, downfall, and rebirth are what fueled the organization to continue pursuing its mission through ups and downs, crests and troughs. </p>
-              <p className="mt-3">Moreso, navigating through a world that continues to progress and circumstances that continue to evolve, we find ourselves reaching out for our ultimate purpose. </p>
-              <p className="mt-3">As it evolved over the past 37 years, the organization has become a premium vehicle for student leaders to advance their contribution to the university mission and to society - Para sa Ateneo, Para sa Lipunan! </p>
-              <p className="mt-3">PIGLASAPAT, rooted in its People, Passion, and Principle, answers today’s call of the times by actively seeking for that purpose — one that is shaped by the realities of the collective and grounded upon the aspirations of what we could become as a community. In introducing fresh traditions, stronger passion projects, and new spaces for service, we thrust forward the Phoenix agenda. We now live on a legacy to create new stories and cultures that will continue to be set in motion for even more glorious years. </p>
-              <p className="mt-3">But, a true PIGLASAPAT leader is bold. So, we don’t just run towards our purpose — we chase it. We chase, even if it means going against the rapids. We chase, even if it means moving mountains. We chase, no matter how difficult the circumstances could get. A deviant from the status quo, a challenger of the norm, a  trailblazer of society — this is the Phoenix spirit we live for. </p>
-              <p className="mt-3 mb-5">On our 37th year, we venture forward to the endless possibilities ahead of us while remaining rooted to our biggest why’s. Today, we no longer wait for the perfect chances. We no longer sit idle in anticipation for change. Together, we <span className="font-header text-yellow"> #ChaseOurPurpose. </span></p>
-            </div>
-
           </div>
-      </section>
+          <Image
+            src="/images/about_footer.jpg"
+            width={500}
+            height={500}
+            alt="cop image 3"
+            priority={true}
+            unoptimized
+            style={{ height: '100%'}}
+          />
+        </section>
+      </Background>
     </div>
   )
 }
--- pglspt-37/components/carousel.jsx
+++ pglspt-37/components/carousel.jsx
@@ -1,100 +1,92 @@
-'use client'
+"use client"
 
 import { Carousel, Typography } from "@material-tailwind/react";
-import Image from "next/image";
  
 export default function Slider() {
   return (
-    <div className="w-fit h-auto">
-        <Carousel className="">
-        {/* Slide 1*/}
-        <div className="relative h-[550px] w-fit overflow-y-hidden">
-            <Image
-                src="/images/carousel_1.jpg"
-                alt="image 1"
-                width={1660}
-                height={550}
-                style={{objectFit:"cover"}}
-            />
-            <div className="absolute inset-0 grid h-full w-full place-items-center bg-[#172554]/50">
-            <div className="w-3/4 text-center md:w-2/4">
-                <Typography
-                variant="h1"
-                className="text-3xl md:text-4xl lg:text-6xl font-header text-yellow"
-                >
-                People
-                </Typography>
-                <Typography
-                color="white"
-                className="font-body text-md md:text-2xl text-justify"
-                >
-                We hone our members to be people equipped with various 
-                capacities and potentials who will remain in mission 
-                to serve the Ateneo and the nation.
-                </Typography>
-            </div>
-            </div>
-        </div>
-
-        {/* Slide 2 */}
-        <div className="relative h-[550px] w-full overflow-y-hidden">
-            <Image
-                src="/images/carousel2.jpg"
-                alt="image 2"
-                width={1660}
-                height={550}
-                style={{objectFit:"cover"}}
-                // className="blur"
-            />
-            <div className="absolute inset-0 grid h-full w-full place-items-center bg-[#172554]/50">
-            <div className="w-3/4 text-center md:w-2/4">
+    <Carousel className="rounded-xl">
+      <div className="relative h-full md:h-[540px] w-full">
+        <img
+          src="/images/carousel_1.jpg"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
             <Typography
-                variant="h1"
-                className="text-3xl md:text-4xl lg:text-6xl font-header text-yellow"
-                >
-                Passion
+              variant="h1"
+              className="text-3xl md:text-4xl lg:text-5xl text-yellow"
+            >
+              People
             </Typography>
             <Typography
-                color="white"
-                className="font-body text-md md:text-2xl text-justify"
-                >
-                We keep the fire of our reimagined aspirations 
-                burning from a selfless place of love for the country and for the community.
+              variant="lead"
+              color="white"
+              className="text-lg md:text-2xl"
+            >
+              We hone our members to be people equipped with various capacities and potentials who will remain in mission to serve the Ateneo and the nation.
             </Typography>
+            <div className="flex justify-center gap-2">
             </div>
-            </div>
+          </div>
         </div>
+      </div>
 
-        {/* Slide 3 */}
-        <div className="relative h-[550px] w-full overflow-y-hidden">
-            <Image
-                src="/images/carousel_3.jpg"
-                alt="image 3"
-                width={1660}
-                height={550}
-                style={{objectFit:"cover"}}
-                // className="blur"
-            />
-            <div className="absolute inset-0 grid h-full w-full place-items-center bg-[#172554]/50">
-            <div className="w-3/4 text-center md:w-2/4">
+      <div className="relative h-full md:h-[540px] w-full">
+        <img
+          src="/images/carousel2.jpg"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
             <Typography
-                variant="h1"
-                className="text-3xl md:text-4xl lg:text-6xl font-header text-yellow"
-                >
-                Principles
-                </Typography>
-                <Typography
-                color="white"
-                className="font-body text-md md:text-2xl text-justify"
-                >
-                We ground ourselves on our core principles of active non-violence, 
-                authentic Christian humanism, liberating faith, 
-                pro-people democracy, and faith that does justice.
-                </Typography>
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl text-yellow"
+            >
+              Passion
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="text-lg md:text-2xl"
+            >
+                We keep the fire of our reimagined aspirations burning from a selfless place of love for the country and for the community.
+            </Typography>
+            <div className="flex justify-center gap-2">
             </div>
-            </div>
+          </div>
         </div>
-        </Carousel>
-    </div>
+      </div>
+
+      <div className="relative h-full md:h-[540px] w-full">
+        <img
+          src="/images/carousel_3.jpg"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl text-yellow"
+            >
+              Principles
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="text-lg md:text-2xl"
+            >
+                We ground ourselves on our core principles of active non-violence, authentic Christian humanism, liberating faith, pro-people democracy, and faith that does justice.
+            </Typography>
+            <div className="flex justify-center gap-2">
+            </div>
+          </div>
+        </div>
+      </div>
+    </Carousel>
   );
 }
