ac37d06ea31bf694c14df611b4ff2cf45fbe3232
Yancy <yancy@symph.co>
2023-09-09 17:50:20 +0800

    added revisions under liyab page

--- pglspt-37/app/liyab/page.js
+++ pglspt-37/app/liyab/page.js
@@ -4,12 +4,22 @@ import Background from '@/components/Background';
 import bg from '@/public/images/backgroundImages/liyabbg.jpg'
 import LiyabCard from './LiyabCard';
 import data from '@/data/liyabData.json'
+import Image from 'next/image';
 
 export default function Liyab() {
   return (
     <Background bgImg={bg}>
-      <div className='header_1 text-center mt-8 mb-14'>
-        LIYAB
+      <div className='flex flex-col px-8 header_1 justify-center items-center mt-8 mb-14'>
+        <Image
+            src="/images/LiyabLogoWhite.png"
+            width={500}
+            height={500}
+            alt="liyab logo"
+            priority={true}
+            unoptimized
+            className="block" 
+            style={{ height: '100%'}}
+          />
         <div className='subheader_1 text-center mt-3'>
           THE OFFICIAL PIGLASAPAT PUBLICATIONS
         </div>
